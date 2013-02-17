b8aa489ab8bd94606283a02d15ccba9183238ab1
pete <p.reisinger@gmail.com>
2013-02-17 20:20:47 +0000

    added hibernate validator and newes pipeline client

---
+++ src/main/java/sk/openhouse/automation/pipelineui/controller/ProjectSettingsController.java
@@ -0,0 +1,11 @@
+package sk.openhouse.automation.pipelineui.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pete
+ * Date: 13/02/13
+ * Time: 19:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectSettingsController {
+}
---
+++ src/main/java/sk/openhouse/automation/pipelineui/controller/VersionsSettingsController.java
@@ -0,0 +1,11 @@
+package sk.openhouse.automation.pipelineui.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pete
+ * Date: 14/02/13
+ * Time: 08:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class VersionsSettingsController {
+}
